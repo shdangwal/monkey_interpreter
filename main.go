e79242b3298d33bd40ccf30bb4c28d09b22124d6
Shubham Dangwal <shdangwal@gmail.com>
2024-10-10 23:19:24 +0530

    completed 2 chapter

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Monkey progarmming language!\n", user.Username)
+	fmt.Printf("Hello %s! This is the Monkey progarmming language!\n",
+		user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
